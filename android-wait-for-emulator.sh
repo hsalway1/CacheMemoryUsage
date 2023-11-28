40fd631d9c07be697aec7f42573e9adf7104b4dc
hsalway1 <62750891+hsalway1@users.noreply.github.com>
2023-11-28 12:47:07 +0530

    Create android-wait-for-emulator.sh

---
+++ android-wait-for-emulator.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+timeout_in_sec=600 # Adjust the timeout duration as needed
+
+elapsed_time=0
+
+until [ $elapsed_time -ge $timeout_in_sec ];
+do
+  if [ "$(adb devices | grep emulator)" ]; then
+    echo "Emulator is ready!"
+    exit 0
+  fi
+
+  sleep 5
+  ((elapsed_time+=5))
+done
+
+echo "Timed out waiting for the emulator to start"
+exit 1
